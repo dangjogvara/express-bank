e7887a3617cdcd28feedba00bc91bb8df2f30d14
Dan Poulsen <dangjogvara@gmail.com>
2023-02-10 19:58:31 +0000

    fix import

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const dbConnect = require('./config/dbConnect');
+const dbConnect = require('../config/dbConnect');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
